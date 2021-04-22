91ee3e6ca6e95177f46d39677ae97bceb54c86a5
Kyle Givler <xvegan88x@gmail.com>
2021-04-21 20:34:03 -0400

    Random Card Picker
    Tried to improve on the example in the book

---
+++ PickRandomCards/PickRandomCards/Card.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PickRandomCards
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string Value { get; set; }
+        public string DisplayName => $"{Value} of {Suit}";
+    }
+}
---
+++ PickRandomCards/PickRandomCards/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Improvement over the book example :D
+// It is only Chapter 3 after all o_O
+
+namespace PickRandomCards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of cards to pick: ");
+            string consoleInput = Console.ReadLine();
+
+            if(int.TryParse(consoleInput, out int numberOfCards))
+            {
+                OutputRandomCards(numberOfCards);
+            }
+            else
+            {
+                Console.WriteLine($"{consoleInput} is not a valid number! Please enter a valid number!");
+                Main(args);
+            }
+        }
+
+        private static void OutputRandomCards(int numberOfCards)
+        {
+            Card[] cardArray = CardPicker.PickSomeCards(numberOfCards);
+            List<Card> cardList = CardPicker.PickSomeCardsList(numberOfCards);
+
+            Console.WriteLine("Array of Random Card Objects: ");
+            foreach(Card c in cardArray)
+            {
+                Console.WriteLine(c.DisplayName);
+            }
+
+            Console.WriteLine("\nList of Random Card Objects: ");
+            foreach (Card c in cardList)
+            {
+                Console.WriteLine(c.DisplayName);
+            }
+        }
+    }
+}
