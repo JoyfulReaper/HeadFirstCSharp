37c36ad95c0bff6164ea20fc2c46250aadaa5c9f
Kyle Givler <xvegan88x@gmail.com>
2021-04-22 21:11:53 -0400

    Casino Demo

---
+++ Casino/Casino/Guy.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public class Guy
+    {
+        private decimal _cash;
+
+        public string Name { get; set; }
+        public decimal Cash
+        {
+            get => _cash;
+            set
+            {
+                if(value < 0)
+                {
+                    GiveCash(value);
+                }
+                else
+                {
+                    ReceiveCash(value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Writes name and amount of cash to the console
+        /// </summary>
+        public void WriteMyInfo()
+        {
+            Console.WriteLine(ToString());
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} has {Cash:C2} bucks.";
+        }
+
+        /// <summary>
+        /// Receive some cash, adding it to my wallet (or thowing ArgumentException)
+        /// </summary>
+        /// <param name="value">Amount of cash to receive</param>
+        public void ReceiveCash(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentException($"{Name} says: {amount} isn't an amount I'll take");
+            }
+
+            _cash += amount;
+        }
+
+        /// <summary>
+        /// Give some of the cash away, removing it from the wallet (Cash amount)
+        /// Throws an ArgumentException if I'm broke
+        /// </summary>
+        /// <param name="value">Amount of cash to give away</param>
+        /// <returns>
+        /// The amount of cash removed from the wallet
+        /// </returns>
+        public decimal GiveCash(decimal amount)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentException($"{Name} says: {amount} isn't a valid amount");
+            }
+
+            if(amount > Cash)
+            {
+                throw new ArgumentException($"{Name} says: I don't have enough cash to give you {amount}");
+            }
+
+            _cash -= amount;
+            return amount;
+        }
+    }
+}
---
+++ Casino/Casino/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Casino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            double odds = 0.75;
+            Guy player = new Guy() { Name = "The Player", Cash = 100 };
+
+            int wins = 0;
+            int losses = 0;
+
+            ConsoleColor color = Console.ForegroundColor;
+
+            Console.WriteLine($"Welcome to The Casino. The odds are {odds:f2}");
+            Console.WriteLine();
+
+            while (player.Cash > 0)
+            {
+                player.WriteMyInfo();
+
+                string howMuch = string.Empty;
+                int amount;
+                while(!int.TryParse(howMuch, out amount) || amount < 1 || amount > player.Cash)
+                {
+                    Console.Write("How much do you want to bet: ");
+                    howMuch = Console.ReadLine();
+                }
+
+                decimal pot = player.GiveCash(amount) * 2;
+
+                if(random.NextDouble() > odds)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"You win {pot:C2}");
+                    Console.ForegroundColor = color;
+                    player.ReceiveCash(pot);
+
+                    wins++;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Bad luck, you lose");
+                    Console.ForegroundColor = color;
+
+                    losses++;
+                }
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine();
+            Console.WriteLine("The house always wins");
+            Console.ForegroundColor = color;
+
+            Console.WriteLine();
+            Console.WriteLine($"You won {wins} rounds and lost {losses} rounds ({(((double)wins/losses) * 100):F2}% win rate)");
+        }
+    }
+}
