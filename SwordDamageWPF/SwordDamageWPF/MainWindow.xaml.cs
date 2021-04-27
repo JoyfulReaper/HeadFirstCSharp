a27f18252b208bb209a311bb4dfbcb8349e25fba
Kyle Givler <xvegan88x@gmail.com>
2021-04-27 18:33:32 -0400

    WPF version, take 1

---
+++ SwordDamageWPF/SwordDamageWPF/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SwordDamageWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Random _random = new Random();
+        SwordDamage _swordDamage = new SwordDamage();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _swordDamage.SetMagic(false);
+            _swordDamage.SetFlaming(false);
+            RollDice();
+        }
+
+        public void RollDice()
+        {
+            _swordDamage.Roll = _random.Next(1, 7) + _random.Next(1, 7) + _random.Next(1, 7);
+            _swordDamage.SetFlaming(flaming.IsChecked.Value);
+            _swordDamage.SetMagic(magic.IsChecked.Value);
+            DisplayDamage();
+        }
+
+        void DisplayDamage()
+        {
+            damage.Text = "Rolled " + _swordDamage.Roll + " for " + _swordDamage.Damage + " HP";
+        }
+
+        private void flaming_Checked(object sender, RoutedEventArgs e)
+        {
+            _swordDamage.SetFlaming(true);
+            DisplayDamage();
+        }
+
+        private void flaming_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _swordDamage.SetFlaming(false);
+            DisplayDamage();
+        }
+
+        private void magic_Checked(object sender, RoutedEventArgs e)
+        {
+            _swordDamage.SetMagic(true);
+            DisplayDamage();
+        }
+
+        private void magic_Unchecked(object sender, RoutedEventArgs e)
+        {
+            _swordDamage.SetMagic(false);
+            DisplayDamage();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            RollDice();
+        }
+    }
+}
