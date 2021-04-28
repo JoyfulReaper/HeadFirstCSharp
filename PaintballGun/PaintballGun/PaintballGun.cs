70e0cf405a8f2b2972c2585aa1f9cc46caef4879
Kyle Givler <xvegan88x@gmail.com>
2021-04-28 16:37:04 -0400

    Painball gun demo

---
+++ PaintballGun/PaintballGun/PaintballGun.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintballGun
+{
+    // What is this, java?
+    // LOL I know its a mess, the book takes you through a look of revisons
+    // PS it is al awesome book, wish I would have found it when I was 1st 
+    // starting out with C#!
+
+    class PaintballGun
+    {
+        public PaintballGun(int balls, int magazineSize, bool loaded) // the bool for loaded seems to work backwards from what I expect, no idea why the author did this...
+        {
+            this._balls = balls; // the book has this.balls = balls, but I prefix locals with _here..
+            MagazineSize = magazineSize;
+            if(!loaded)
+            {
+                Reload();
+            }
+        }
+
+        //public const int MAGAZINE_SIZE = 16;
+        public int MagazineSize { get; private set; } = 16;
+
+        private int _balls = 0;
+        //private int _ballsLoaded = 0;
+        public int BallsLoaded { get; private set; }
+
+        public int Balls
+        {
+            get => _balls;
+            set
+            {
+                if (value > 0)
+                {
+                    _balls = value;
+                }
+                Reload();
+            }
+        }
+
+        //public int BallsLoaded { get => _ballsLoaded; set { _ballsLoaded = value; } }
+
+        //public int GetBallsLoaded() => _ballsLoaded;
+
+        public bool IsEmpty() => BallsLoaded == 0;
+
+        //public int GetBalls() => balls;
+
+        //public void SetBalls(int numberOfBalls)
+        //{
+        //    if(numberOfBalls > 0)
+        //    {
+        //        balls = numberOfBalls;
+        //    }
+
+        //    Reload();
+        //}
+
+        public void Reload()
+        {
+            //if(_balls > MAGAZINE_SIZE)
+            //{
+            //    BallsLoaded = MAGAZINE_SIZE;
+            //}
+            if (_balls > MagazineSize)
+            {
+                BallsLoaded = MagazineSize;
+            }
+            else
+            {
+                BallsLoaded = _balls;
+            }
+        }
+
+        public bool Shoot()
+        {
+            if(BallsLoaded == 0)
+            {
+                return false;
+            }
+
+            BallsLoaded--;
+            _balls--;
+
+            return true;
+        }
+    }
+}
---
+++ PaintballGun/PaintballGun/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace PaintballGun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfBalls = ReadInt(20, "Number of balls");
+            int magazineSize = ReadInt(16, "Magazine size");
+
+            Console.Write("Loaded [false]: ");
+            bool.TryParse(Console.ReadLine(), out bool isLoaded);
+
+            PaintballGun gun = new PaintballGun(numberOfBalls, magazineSize, isLoaded);
+
+            while(true)
+            {
+                //Console.WriteLine($"{gun.GetBalls()} balls, {gun.GetBallsLoaded()} loaded");
+                Console.WriteLine($"{gun.Balls} balls, {gun.BallsLoaded} loaded");
+                if (gun.IsEmpty())
+                {
+                    Console.WriteLine("WARNING: You're out of ammo");
+                }
+
+                Console.WriteLine("Space to shoot, r to reload, + to add ammon, q to quit");
+
+                char key = Console.ReadKey(true).KeyChar;
+                
+                switch(key)
+                {
+                    case ' ':
+                        Console.WriteLine($"Shooting returned {gun.Shoot()}");
+                        break;
+                    case 'r':
+                        gun.Reload();
+                        break;
+                    case '+':
+                        //gun.SetBalls(gun.GetBalls() + PaintballGun.MAGAZINE_SIZE);
+                        gun.Balls += gun.MagazineSize;
+                        break;
+                    case 'q':
+                        return;
+                }
+            }
+        }
+
+        static int ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.Write(prompt + " [" + lastUsedValue + "]: ");
+            string line = Console.ReadLine();
+            if (int.TryParse(line, out int value))
+            {
+                Console.WriteLine("   using value " + value);
+                return value;
+            }
+            else
+            {
+                Console.WriteLine("   using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
