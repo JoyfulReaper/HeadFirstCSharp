4db8803150bfe1f70590ae7bca51532412c69d02
Kyle Givler <xvegan88x@gmail.com>
2021-05-02 15:57:47 -0400

    No interfaces

---
+++ HippoAndWolf/HippoAndWolf/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HippoAndWolf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Animal> animals = new List<Animal>()
+            {
+                new Wolf(false),
+                new Hippo(),
+                new Wolf(true),
+                new Wolf(false),
+                new Hippo()
+            };
+
+            foreach (Animal animal in animals)
+            {
+                animal.MakeNoise();
+
+                if(animal is Hippo hippo)
+                {
+                    hippo.Swim();
+                }
+
+                if(animal is Wolf wolf)
+                {
+                    wolf.HuntInPack();
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
