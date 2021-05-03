ad1fe5c54920573b8f960dda5f92cb2d7d59f759
Kyle Givler <xvegan88x@gmail.com>
2021-05-03 18:43:19 -0400

    Static property and methods in an interface

--- ScaryClown/ScaryClown/IClown.cs
+++ ScaryClown/ScaryClown/IClown.cs
@@ -8,7 +8,35 @@ namespace ScaryClown
 {
     interface IClown
     {
+        public static int CarCapacity
+        {
+            get
+            {
+                return _carCapacity;
+            }
+            set
+            {
+                if (value > 10)
+                {
+                    _carCapacity = value;
+                }
+                else
+                {
+                    throw new ArgumentException($"Warning: Car capacity {value} is too small", nameof(value));
+                }
+            }
+        }
+
+        private static int _carCapacity;
+
+        protected static Random _random = new Random();
+
         public string FunnyThingIHave { get; }
         void Honk();
+
+        public static string ClownCarDescription()
+        {
+            return $"A clown car with {_random.Next(_carCapacity / 2, _carCapacity)} clowns";
+        }
     }
 }
--- ScaryClown/ScaryClown/Program.cs
+++ ScaryClown/ScaryClown/Program.cs
@@ -6,6 +6,9 @@ namespace ScaryClown
     {
         static void Main(string[] args)
         {
+            IClown.CarCapacity = 18;
+            Console.WriteLine(IClown.ClownCarDescription());
+
             IClown fingersTheClown = new ScaryScary("big red nose", 14);
             fingersTheClown.Honk();
 
