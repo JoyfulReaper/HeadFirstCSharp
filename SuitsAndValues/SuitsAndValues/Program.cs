5137b6e2910ee0b78f2e9724fc0d3c83e266eaee
Kyle Givler <xvegan88x@gmail.com>
2021-05-04 19:38:30 -0400

    Suits and Values, Deck with an Array o_O

---
+++ SuitsAndValues/SuitsAndValues/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SuitsAndValues
+{
+    class Program
+    {
+        private static Random _random = new Random();
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1; i++)
+            {
+                Card card = new Card((Values)_random.Next((int)Values.Ace, (int)Values.King + 1), (Suits)_random.Next((int)Suits.Diamonds, (int)Suits.Spades + 1));
+                Console.WriteLine(card);
+            }
+
+            Console.WriteLine();
+            Deck deck = new Deck();
+            deck.PrintCards();
+        }
+    }
+}
