08ce0be10b04fa2bc6f6f00c99e24248769eaa06
Kyle Givler <xvegan88x@gmail.com>
2021-05-05 17:47:58 -0400

    Add Main

--- Shoes/Shoes/Program.cs
+++ Shoes/Shoes/Program.cs
@@ -4,9 +4,27 @@ namespace Shoes
 {
     class Program
     {
+        static readonly ShoeCloset _shoeCloset = new();
         static void Main(string[] args)
         {
-            // Main will go here later :p
+            while (true)
+            {
+                _shoeCloset.PrintShoes();
+                Console.Write("\nPress 'a' to add or 'r' to remove a shoe: ");
+                char key = Char.ToUpperInvariant(Console.ReadKey().KeyChar);
+
+                switch (key)
+                {
+                    case 'A':
+                        _shoeCloset.AddShoe();
+                        break;
+                    case 'R':
+                        _shoeCloset.RemoveShoe();
+                        break;
+                    default:
+                        return;
+                }
+            }
         }
     }
 }
