24fa3f35fa55146ab9722d37ae42060246ddcde3
Kyle Givler <xvegan88x@gmail.com>
2021-05-20 19:50:26 -0400

    Think I fixed all the bugs in the code in the book also :D

---
+++ TwoDecksBlazor/TwoDecksBlazor/Direction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoDecksBlazor
+{
+    enum Direction
+    {
+        LeftToRight,
+        RightToLeft,
+    }
+}
--- TwoDecksBlazor/TwoDecksBlazor/TwoDecks.cs
+++ TwoDecksBlazor/TwoDecksBlazor/TwoDecks.cs
@@ -7,23 +7,73 @@ namespace TwoDecksBlazor
 {
     class TwoDecks
     {
-        private Deck leftDeck = new Deck();
-        public int LeftDeckCount { get => leftDeck.Count; }
+        private  Deck _leftDeck = new Deck();
+        private  Deck _rightDeck = new Deck();
+
+        public int LeftDeckCount { get => _leftDeck.Count; }
         public int LeftCardSelected { get; set; }
+
+        public int RightDeckCount { get => _rightDeck.Count; }
+        public int RightCardSelected { get; set; }
 
         public string LeftDeckCardName(int i)
         {
-            return leftDeck[i].ToString();
+            if (_leftDeck.Count == 0 || i < 0 || i >= _leftDeck.Count)
+            {
+                return string.Empty;
+            }
+
+            return _leftDeck[i].ToString();
+        }
+
+        public string RightDeckCardName(int i)
+        {
+            if(_rightDeck.Count == 0 || i < 0 || i >= _rightDeck.Count)
+            {
+                return string.Empty;
+            }
+
+            return _rightDeck[i].ToString();
         }
 
         public void Shuffle()
         {
-            leftDeck.Shuffle();
+            _leftDeck.Shuffle();
         }
 
         public void Reset()
         {
-            leftDeck = new Deck();
+            _leftDeck = new Deck();
+        }
+
+        public void Sort()
+        {
+            _rightDeck.Sort(new CardComparerByValue());
+        }
+
+        public void Clear()
+        {
+            _rightDeck.Clear();
+        }
+
+        public void MoveCard(Direction direction)
+        {
+            if(direction == Direction.LeftToRight)
+            {
+                if (_leftDeck.Count == 0 || LeftCardSelected >= _leftDeck.Count)
+                    return;
+
+                _rightDeck.Add(_leftDeck[LeftCardSelected]);
+                _leftDeck.RemoveAt(LeftCardSelected);
+            }
+            else
+            {
+                if (_rightDeck.Count == 0 || RightCardSelected >= _rightDeck.Count)
+                    return;
+
+                _leftDeck.Add(_rightDeck[RightCardSelected]);
+                _rightDeck.RemoveAt(RightCardSelected);
+            }
         }
     }
 }
