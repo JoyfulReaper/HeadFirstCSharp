7a1a8ddf65678e4e95aef1ec9aa82d4d43715e29
Kyle Givler <xvegan88x@gmail.com>
2021-05-20 18:26:25 -0400

    TwoDecksBlazor Part 1

---
+++ TwoDecksBlazor/TwoDecksBlazor/Deck.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoDecksBlazor
+{
+    class Deck : List<Card>
+    {
+        private static readonly Random _random = new Random();
+
+        public Deck()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            Clear();
+
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 1; j <= 13; j++)
+                {
+                    Add(new Card((Values)j, (Suits)i));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            List<Card> copy = new List<Card>(this);
+            Clear();
+
+            while(copy.Count > 0)
+            {
+                var index = _random.Next(copy.Count);
+                var card = copy[index];
+                copy.RemoveAt(index);
+                Add(card);
+            }
+        }
+
+        public Card Deal (int index)
+        {
+            var card = base[index];
+            RemoveAt(index);
+            return card;
+        }
+    }
+}
---
+++ TwoDecksBlazor/TwoDecksBlazor/TwoDecks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TwoDecksBlazor
+{
+    class TwoDecks
+    {
+        private Deck leftDeck = new Deck();
+        public int LeftDeckCount { get => leftDeck.Count; }
+        public int LeftCardSelected { get; set; }
+
+        public string LeftDeckCardName(int i)
+        {
+            return leftDeck[i].ToString();
+        }
+
+        public void Shuffle()
+        {
+            leftDeck.Shuffle();
+        }
+
+        public void Reset()
+        {
+            leftDeck = new Deck();
+        }
+    }
+}
