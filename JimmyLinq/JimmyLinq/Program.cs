424040f500d7c752be17534bf1b1b51cb83b835a
Kyle Givler <xvegan88x@gmail.com>
2021-06-02 18:49:33 -0400

    Same thing a different way

--- JimmyLinq/JimmyLinq/Program.cs
+++ JimmyLinq/JimmyLinq/Program.cs
@@ -18,6 +18,19 @@ namespace JimmyLinq
             {
                 Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
             }
+
+            Console.WriteLine();
+
+            IEnumerable<string> mostExpensiveDescription =
+                from comic in Comic.Catalog
+                where Comic.Prices[comic.Issue] > 500
+                orderby Comic.Prices[comic.Issue] descending
+                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+
+            foreach (string comic in mostExpensiveDescription)
+            {
+                Console.WriteLine($"{comic}");
+            }
         }
     }
 }
