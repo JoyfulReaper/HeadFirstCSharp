1903255faa38d5b0e9bd71d371349a2b0f740c75
Kyle Givler <xvegan88x@gmail.com>
2021-06-02 18:58:12 -0400

    messin with linq

--- JimmyLinq/JimmyLinq/Program.cs
+++ JimmyLinq/JimmyLinq/Program.cs
@@ -8,6 +8,9 @@ namespace JimmyLinq
     {
         static void Main(string[] args)
         {
+            TheOtherDemo();
+            Console.WriteLine();
+
             IEnumerable<Comic> mostExpensive =
                 from comic in Comic.Catalog
                 where Comic.Prices[comic.Issue] > 500
@@ -30,6 +33,25 @@ namespace JimmyLinq
             foreach (string comic in mostExpensiveDescription)
             {
                 Console.WriteLine($"{comic}");
+            }
+        }
+
+        private static void TheOtherDemo()
+        {
+            Random rand = new();
+
+            var sandwiches = new[] { "ham and cheese", "salami with mayo", "turkey and swiss", "chicken cutlet" };
+            var breads = new[] { "rye", "white", "wheat", "sour dough", "pumpernickel" };
+
+            var bread = breads[rand.Next(0, breads.Length)];
+
+            var todaysSpecial =
+                from sandwich in sandwiches
+                select $"{sandwich} on {bread}";
+
+            foreach(var s in todaysSpecial)
+            {
+                Console.WriteLine(s);
             }
         }
     }
