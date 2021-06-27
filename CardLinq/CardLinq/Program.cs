ab88985f723e922e53142d08e7da4cddbe82384f
Kyle Givler <xvegan88x@gmail.com>
2021-06-27 15:05:23 -0400

    Lamda/Linq Challenge

--- CardLinq/CardLinq/Program.cs
+++ CardLinq/CardLinq/Program.cs
@@ -5,8 +5,32 @@ namespace CardLinq
 {
     class Program
     {
+        private static string Output(Suits suit, int number) =>
+            $"Suit is {suit} and number is {number}";
+
         static void Main(string[] args)
         {
+
+            var deck3 = new Deck();
+            var processedCards = deck3
+                .Take(3)
+                .Concat(deck3.TakeLast(3))
+                .OrderByDescending(card => card)
+                .Select(card => card.Value switch
+                {
+                    Values.King => Output(card.Suit, 7),
+                    Values.Ace => $"It's an ace! {card.Suit}",
+                    Values.Jack => Output((Suits)card.Suit -1, 9),
+                    Values.Two => Output(card.Suit, 18),
+                    _ => card.ToString(),
+                });
+
+            foreach(var output in processedCards)
+            {
+                Console.WriteLine(output);
+            }
+            return;
+            /////////////////////////////
             var deck = new Deck()
                 .Shuffle()
                 .Take(16);
