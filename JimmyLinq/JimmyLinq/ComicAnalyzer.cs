d39cd0a0f8608c52e77dc8f56b9a532bcf63deeb
Kyle Givler <xvegan88x@gmail.com>
2021-06-27 15:31:28 -0400

    Switch statement example

--- JimmyLinq/JimmyLinq/ComicAnalyzer.cs
+++ JimmyLinq/JimmyLinq/ComicAnalyzer.cs
@@ -20,23 +20,36 @@ namespace JimmyLinq
 
         public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
         {
-            var groupedComics = 
-                from comic in comics
-                orderby prices[comic.Issue]
-                group comic by CalculatePriceRange(comic, prices) into PriceGroup
-                select PriceGroup;
+            //var groupedComics = 
+            //    from comic in comics
+            //    orderby prices[comic.Issue]
+            //    group comic by CalculatePriceRange(comic, prices) into PriceGroup
+            //    select PriceGroup;
+
+            var groupedComics = comics.OrderBy(x => prices[x.Issue])
+                .GroupBy(x => CalculatePriceRange(x, prices));
+
 
             return groupedComics;
         }
 
         public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
         {
+            //var reviewedComics =
+            //    from comic in comics
+            //    orderby comic.Issue
+            //    join review in reviews
+            //    on comic.Issue equals review.Issue
+            //    select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+
             var reviewedComics =
-                from comic in comics
-                orderby comic.Issue
-                join review in reviews
-                on comic.Issue equals review.Issue
-                select $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}";
+                comics.OrderBy(x => x.Issue)
+                .Join(reviews, // sequence to join
+                        comic => comic.Issue, // on part of join
+                        review => review.Issue, // equals part of join
+                        (comic, review) => // two parameters and returns select output
+                        $"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}");
+                
 
             return reviewedComics;
         }
