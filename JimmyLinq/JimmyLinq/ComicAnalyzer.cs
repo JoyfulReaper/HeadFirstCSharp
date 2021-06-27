ebbac0b594a5f3983a847526370f6c5e9a07cb49
Kyle Givler <xvegan88x@gmail.com>
2021-06-27 09:58:33 -0400

    More LINQ examples

---
+++ JimmyLinq/JimmyLinq/ComicAnalyzer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JimmyLinq
+{
+    static class ComicAnalyzer
+    {
+        private static PriceRange CalculatePriceRange(Comic comic)
+        {
+            if (Comic.Prices[comic.Issue] < 100)
+            {
+                return PriceRange.Cheap;
+            }
+
+            return PriceRange.Expensive;
+        }
+
+        public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(IEnumerable<Comic> comics, IReadOnlyDictionary<int, decimal> prices)
+        {
+            var groupedComics = 
+                from comic in comics
+                orderby prices[comic.Issue]
+                group comic by CalculatePriceRange(comic) into PriceGroup
+                select PriceGroup;
+
+            return groupedComics;
+        }
+
+        public static IEnumerable<string> GetReviews(IEnumerable<Comic> comics, IEnumerable<Review> reviews)
+        {
+            var reviewedComics =
+                from comic in comics
+                orderby comic.Issue
+                join review in reviews
+                on comic.Issue equals review.Issue
+                select $"{review.Critic} rated {comic.Issue} '{comic.Name}' {review.Score}";
+
+            return reviewedComics;
+        }
+    }
+}
--- JimmyLinq/JimmyLinq/Program.cs
+++ JimmyLinq/JimmyLinq/Program.cs
@@ -8,34 +8,77 @@ namespace JimmyLinq
     {
         static void Main(string[] args)
         {
-            AnotherDemo();
-            Console.WriteLine();
-            TheOtherDemo();
-            Console.WriteLine();
-
-            IEnumerable<Comic> mostExpensive =
-                from comic in Comic.Catalog
-                where Comic.Prices[comic.Issue] > 500
-                orderby Comic.Prices[comic.Issue] descending
-                select comic;
-
-            foreach(Comic comic in mostExpensive)
+            var done = false;
+            while(!done)
             {
-                Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+                Console.WriteLine("\nPress G to group comics by price, R to get reviews, any other key to quit\n");
+                switch (Console.ReadKey(true).KeyChar.ToString().ToUpper())
+                {
+                    case "G":
+                        done = GroupComicsByPrice();
+                        break;
+                    case "R":
+                        done = GetReviews();
+                        break;
+                    default:
+                        done = true;
+                        break;
+                }
             }
 
-            Console.WriteLine();
+            ///////////////////////////////////////////////////
+            //AnotherDemo();
+            //Console.WriteLine();
+            //TheOtherDemo();
+            //Console.WriteLine();
 
-            IEnumerable<string> mostExpensiveDescription =
-                from comic in Comic.Catalog
-                where Comic.Prices[comic.Issue] > 500
-                orderby Comic.Prices[comic.Issue] descending
-                select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+            //IEnumerable<Comic> mostExpensive =
+            //    from comic in Comic.Catalog
+            //    where Comic.Prices[comic.Issue] > 500
+            //    orderby Comic.Prices[comic.Issue] descending
+            //    select comic;
 
-            foreach (string comic in mostExpensiveDescription)
+            //foreach(Comic comic in mostExpensive)
+            //{
+            //    Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:c}");
+            //}
+
+            //Console.WriteLine();
+
+            //IEnumerable<string> mostExpensiveDescription =
+            //    from comic in Comic.Catalog
+            //    where Comic.Prices[comic.Issue] > 500
+            //    orderby Comic.Prices[comic.Issue] descending
+            //    select $"{comic} is worth {Comic.Prices[comic.Issue]:c}";
+
+            //foreach (string comic in mostExpensiveDescription)
+            //{
+            //    Console.WriteLine($"{comic}");
+            //}
+        }
+
+        private static bool GroupComicsByPrice()
+        {
+            var groups = ComicAnalyzer.GroupComicsByPrice(Comic.Catalog, Comic.Prices);
+            foreach(var group in groups)
             {
-                Console.WriteLine($"{comic}");
+                Console.WriteLine($"{group.Key} comics:");
+                foreach(var comic in group)
+                {
+                    Console.WriteLine($"#{comic.Issue} {comic.Name}: {Comic.Prices[comic.Issue]:c}");
+                }
             }
+            return false;
+        }
+
+        private static bool GetReviews()
+        {
+            var reviews = ComicAnalyzer.GetReviews(Comic.Catalog, Comic.Reviews);
+            foreach(var review in reviews)
+            {
+                Console.WriteLine(review);
+            }
+            return false;
         }
 
         private static void AnotherDemo()
