bc26d6fb07a443ac3b0c3de948ea528eb4757543
Kyle Givler <xvegan88x@gmail.com>
2021-06-27 14:34:02 -0400

    LIN Q!

--- CardLinq/CardLinq/Program.cs
+++ CardLinq/CardLinq/Program.cs
@@ -17,7 +17,23 @@ namespace CardLinq
                 orderby suitGroup.Key descending
                 select suitGroup;
 
+            var deck2 = new Deck();
+            var grouped2 =
+                deck2.Shuffle()
+                .Take(16)
+                .GroupBy(x => x.Suit)
+                .OrderByDescending(x => x.Key);
+
             foreach (var group in grouped)
+            {
+                Console.WriteLine($@"Group: {group.Key}
+Count: {group.Count()}
+Minumum: {group.Min()}
+Maximum: {group.Max()}" + "\n");
+            }
+
+            Console.WriteLine("Method chanining: ");
+            foreach (var group in grouped2)
             {
                 Console.WriteLine($@"Group: {group.Key}
 Count: {group.Count()}
