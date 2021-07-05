d66cec9cdb2b3f63420ff4e45ba0e8d67b587645
Kyle Givler <xvegan88x@gmail.com>
2021-07-04 23:34:36 -0400

    Start Go Fish! Project

---
+++ GoFish/GoFish/GameState.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    public class GameState
+    {
+        public readonly IEnumerable<Player> Players;
+        public readonly IEnumerable<Player> Opponents;
+        public readonly Player HumanPlayer;
+        public bool GameOver { get; private set; } = false;
+
+        public readonly Deck Stock;
+
+        /// <summary>
+        /// Creates the players and deals their first hands
+        /// </summary>
+        /// <param name="humanPlayerName"></param>
+        /// <param name="opponentNames"></param>
+        /// <param name="stock"></param>
+        public GameState(string humanPlayerName, IEnumerable<string> opponentNames, Deck stock)
+        {
+            this.Stock = stock;
+
+            HumanPlayer = new Player(humanPlayerName);
+            HumanPlayer.GetNextHand(Stock);
+
+            var opponents = new List<Player>();
+            foreach(var o in opponentNames)
+            {
+                var player = new Player(o);
+                player.GetNextHand(Stock);
+                opponents.Add(player);
+            }
+            Opponents = opponents;
+
+            Players = new List<Player>() { HumanPlayer }.Concat(Opponents);
+        }
+
+        public Player RandomPlayer(Player currentPlayer) =>
+            Players
+                .Where(player => player != currentPlayer)
+                .Skip(Player.Random.Next(Players.Count() - 1))
+                .First();
+
+        public string PlayRound(Player player, Player playerToAsk,
+            Values valueToAskFor, Deck stock)
+        {
+            var plural = (valueToAskFor == Values.Six) ? "Sixes" : $"{valueToAskFor}s";
+            var message = $"{player} asked {playerToAsk} for {plural}" + Environment.NewLine;
+
+            var cards = playerToAsk.DoYouHaveAny(valueToAskFor, stock);
+            if (cards.Count() > 0)
+            {
+                player.AddCardsAndPullOutBooks(cards);
+                message += $"{playerToAsk} has {cards.Count()} {valueToAskFor} card{Player.S(cards.Count())}";
+            }
+            else
+            {
+                player.DrawCard(stock);
+                message += $"{player} drew a card";
+            }
+
+            if (player.Hand.Count() == 0)
+            {
+                player.GetNextHand(stock);
+                message += $"{Environment.NewLine}{player} ran out of cards," +
+                    $" drew {player.Hand.Count()} from the stock";
+            }
+
+            return message;
+        }
+
+        public string CheckForWinner()
+        {
+            var playerCards = Players.Select(player => player.Hand.Count()).Sum();
+            if (playerCards > 0)
+            {
+                return "";
+            }
+
+            GameOver = true;
+            var WinningBookCount = Players.Select(player => player.Books.Count()).Max();
+            var winners = Players.Where(player => player.Books.Count() == WinningBookCount);
+            if(winners.Count() == 1)
+            {
+                return $"The winner is {winners.First().Name}";
+            }
+
+            return $"The winners are {string.Join(" and ", winners)}";
+        }
+    }
+}
---
+++ GoFish/GoFish/Player.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoFish
+{
+    public class Player
+    {
+        public static Random Random = new Random();
+
+        private List<Card> hand = new List<Card>();
+        private List<Values> books = new List<Values>();
+
+        /// <summary>
+        /// The cards in the player's hand
+        /// </summary>
+        public IEnumerable<Card> Hand => hand;
+
+        /// <summary>
+        /// The books that the player has pulled out
+        /// </summary>
+        public IEnumerable<Values> Books => books;
+
+        public readonly string Name;
+
+        public static string S(int s) => s == 1 ? "" : "s";
+
+        /// <summary>
+        /// Returns the current status of the player: the number of cards and books
+        /// </summary>
+        public string Status => $"{Name} has {hand.Count} card{S(hand.Count)} and {books.Count} book{S(books.Count)}";
+
+        public Player(string name) => Name = name;
+
+        public Player(string name, IEnumerable<Card> cards)
+        {
+            Name = name;
+            hand.AddRange(cards);
+        }
+
+        /// <summary>
+        /// Gets up to five cards from the stock
+        /// </summary>
+        /// <param name="stock"></param>
+        public void GetNextHand(Deck stock)
+        {
+            while(stock.Count > 0 && hand.Count < 5)
+            {
+                hand.Add(stock.Deal(0));
+            }
+        }
+
+        /// <summary>
+        /// If I have any cards that match the value, return them. If I run out of cards, get
+        /// the next hand from the deck.
+        /// </summary>
+        /// <param name="value">Value I am asked for</param>
+        /// <param name="deck">Deck to draw my next hand from</param>
+        /// <returns>The cards that were pulled out of the other player's hand</returns>
+        public IEnumerable<Card> DoYouHaveAny(Values value, Deck deck)
+        {
+            var matchingCards = 
+                hand.Where(card => card.Value == value)
+                .OrderBy(card => card.Suit);
+
+            hand = hand.Where(card => card.Value != value).ToList();
+
+            if(hand.Count < 1)
+            {
+                GetNextHand(deck);
+            }
+
+            return matchingCards;
+        }
+
+        /// <summary>
+        /// When the player recives cards from another player, adds them to the hand
+        /// and pulls out any matching books
+        /// </summary>
+        /// <param name="cards">Cards from the other player to add</param>
+        public void AddCardsAndPullOutBooks(IEnumerable<Card> cards)
+        {
+            hand.AddRange(cards);
+
+            var foundBooks = hand
+                .GroupBy(card => card.Value)
+                .Where(group => group.Count() == 4)
+                .Select(group => group.Key);
+
+            books.AddRange(foundBooks);
+            books.Sort();
+
+            hand = hand.Where(card => !books.Contains(card.Value))
+                .ToList();
+        }
+
+        /// <summary>
+        /// Draws a card from the stock and adds it to the player's hand
+        /// </summary>
+        /// <param name="stock"></param>
+        public void DrawCard(Deck stock)
+        {
+            if(stock.Count > 0)
+            {
+                hand.Add(stock.Deal(0));
+            }
+        }
+
+        public Values RandomValueFromHand() => hand[Random.Next(0, hand.Count)].Value;
+
+        public override string ToString() => Name;
+    }
+}
